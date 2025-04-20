8918179f2caf4b9ce681697156af5bd38d77f9d5
yecohe <cohen.ye@gmail.com>
2025-04-20 12:02:05 +0300

    Update BestEpisodes.tsx

--- src/components/BestEpisodes.tsx
+++ src/components/BestEpisodes.tsx
@@ -54,7 +54,7 @@ const BestEpisodes = () => {
       <div className="container px-6">
         <div className="mb-12">
           <h2 className="text-4xl md:text-5xl font-bold text-podcast-magenta mb-4">פרקים מומלצים</h2>
-          <p className="text-white/80 text-lg">פרקים אהובים עלינו במיוחד</p>
+          <p className="text-white/80 text-lg">האזינו לפרקים שאהובים עלינו במיוחד</p>
         </div>
         
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
