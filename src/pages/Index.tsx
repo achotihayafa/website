9ed38bdd3c86ea23bcfa8d7a269ea91f4c436005
yecohe <cohen.ye@gmail.com>
2025-04-20 11:40:51 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import Navbar from '@/components/Navbar';
 import HeroSection from '@/components/HeroSection';
+import PodcastPlatforms from '@/components/PodcastPlatforms';
 import BestEpisodes from '@/components/BestEpisodes';
 import LatestEpisodes from '@/components/LatestEpisodes';
 import AboutSection from '@/components/AboutSection';
-import PodcastPlatforms from '@/components/PodcastPlatforms';
 import Footer from '@/components/Footer';
 
 const Index = () => {
@@ -13,9 +13,9 @@ const Index = () => {
     <div className="min-h-screen">
       <Navbar />
       <HeroSection />
+      <PodcastPlatforms />
       <BestEpisodes />
       <LatestEpisodes />
-      <PodcastPlatforms />
       <AboutSection />
       <Footer />
     </div>
