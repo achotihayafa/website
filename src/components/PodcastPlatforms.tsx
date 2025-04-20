07d2c7bbbec946c083917326860472ed2dbaae2d
yecohe <cohen.ye@gmail.com>
2025-04-20 12:48:56 +0300

    Update PodcastPlatforms.tsx

--- src/components/PodcastPlatforms.tsx
+++ src/components/PodcastPlatforms.tsx
@@ -1,33 +1,32 @@
-
 import React from 'react';
-import { AppleIcon, YoutubeIcon, RadioIcon, RssIcon, MusicIcon, Headphones } from 'lucide-react';
+import { FaSpotify, SiApplepodcasts, FaYoutube, SiPodcastaddict, SiPocketcasts } from 'react-icons/fa';
 
 const PodcastPlatforms = () => {
   const platforms = [
     {
       name: "Spotify",
       link: "https://open.spotify.com/show/0ZpvzCEuDeKQhBw74YEmp9",
-      icon: <MusicIcon className="h-8 w-8" />
+      icon: <FaSpotify className="h-8 w-8" />
     },
     {
       name: "Apple Podcasts",
       link: "https://podcasts.apple.com/us/podcast/אחותי-היפה/id1728358395",
-      icon: <AppleIcon className="h-8 w-8" />
+      icon: <SiApplepodcasts className="h-8 w-8" />
     },
     {
       name: "YouTube",
       link: "https://www.youtube.com/@AchotiHaYafa",
-      icon: <YoutubeIcon className="h-8 w-8" />
+      icon: <FaYoutube className="h-8 w-8" />
     },
     {
       name: "Podcast Addict",
       link: "https://podcastaddict.com/podcast/%D7%90%D7%97%D7%95%D7%AA%D7%99%20%D7%94%D7%99%D7%A4%D7%94/5306867",
-      icon: <RadioIcon className="h-8 w-8" />
+      icon: <SiPodcastaddict className="h-8 w-8" />
     },
     {
       name: "Pocket Casts",
       link: "https://pca.st/zapd6uv9",
-      icon: <Headphones className="h-8 w-8" />
+      icon: <SiPocketcasts className="h-8 w-8" />
     }
   ];
 
@@ -36,7 +35,7 @@ const PodcastPlatforms = () => {
       <div className="container px-6">
         <h2 className="text-4xl md:text-5xl mb-10 text-center">האזינו לנו</h2>
         
-        <div className="grid grid-cols-4 md:grid-cols-7 gap-8 max-w-3xl mx-auto">
+        <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-8 max-w-4xl mx-auto">
           {platforms.map((platform) => (
             <a 
               key={platform.name} 
