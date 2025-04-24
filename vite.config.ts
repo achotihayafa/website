6aa2da31f30f3098475f9c2a651b09473f48b42a
achotihayafa <achotihayafa@gmail.com>
2025-04-24 09:08:40 +0000

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+// import componentTagger from './path-to-componentTagger' // Uncomment if you have this plugin
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/website/', // ✅ correct placement
   server: {
     host: "::",
     port: 8080,
@@ -14,10 +15,8 @@ export default defineConfig(({ mode }) => ({
       jsxImportSource: 'react',
       tsDecorators: true,
     }),
-    base: '/website/',
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    // mode === 'development' && componentTagger(), // Uncomment if defined
+  ].filter(Boolean), // filters out `false` if in production
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
