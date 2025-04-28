42d393181bf322b9d3a00a2e9036500d0d91964f
achotihayafa <achotihayafa@gmail.com>
2025-04-28 18:07:41 +0000

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/website/', // ✅ correct placement
+  base: '/', // 
   server: {
     host: "::",
     port: 8080,
