ae36a1896c57b0b6b3ab10789e6bbcd5310be38b
achotihayafa <achotihayafa@gmail.com>
2026-01-25 13:34:07 +0000

    github actions

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,    // Required for static export
   reactStrictMode: true,
 };
 
